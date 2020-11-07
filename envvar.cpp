d04f39190ff30649e1be62b2a54f2310630745fa
Bogdan Yakupov <yourinfinityuser@gmail.com>
2020-11-07 10:27:58 +0600

    Fixed window settings

--- envvar.cpp
+++ envvar.cpp
@@ -1,4 +1,4 @@
-sf::RenderWindow window(sf::VideoMode(1, 1), "sandbox");
+sf::RenderWindow window(sf::VideoMode(470, 470), "sandbox");
 sf::RectangleShape tempRect(sf::Vector2f(env::step, env::step));
 sf::Color lightC = sf::Color(255, 255, 0);
 int actionTimer = 1;
